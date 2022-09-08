b687a495b83b7c396b1ca9e9fd67ac013be07f54
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-09-08 20:17:28 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,19 @@
-#include <stdio.h>
-int main (void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
-  /**
- * Write a C program that prints exactly
- * 
- * "Programming is like building a multilingual puzzle"
- * 
- * followed by a new line.
+/*
+ * File: 4-puts.c
+ * Auth: Brennan D Baraban <375@holbertonschool.com>
  */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
