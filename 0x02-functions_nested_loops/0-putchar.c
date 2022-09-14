be3e8d66dd592403c68642c6d8c06d4756a90732
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-09-14 02:21:57 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,16 +13,30 @@
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+#include "main.h"
+
+/**
+
+* main - print putchar
+
+* Return: always 0
+
+*/
+
+int main(void)
+
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+
+return (0)
 
 	return (0);
 }
