fb45333159b37160170f17a0483e17a695ada7d5
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-09-14 02:25:01 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  *
  */
 
-#include "main.h"
+#include "holberton.h"
 
 /**
  * main - Prints "Holberton" followed by a new line.
@@ -13,30 +13,15 @@
  */
 int main(void)
 {
-#include "main.h"
-
-/**
-
-* main - print putchar
-
-* Return: always 0
-
-*/
-
-int main(void)
-
-{
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-
-return (0)
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
