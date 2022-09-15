812d2dddc2639a71726bcd5d362aa9c8bd91f96f
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-09-15 23:23:56 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,23 @@
-1-isdigit.c
+/*
+ * File: 6-print_line.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line using the character _.
+ * @n: The number of _ characters to be printed.
+ */
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
