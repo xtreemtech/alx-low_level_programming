ad2e08b4a77c3476b3227db69ba0eea1d31a00c4
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-09-19 14:56:34 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,23 @@
-0-reset_to_98.c
+/*
+ * File: 4-print_rev.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
+void print_rev(char *s)
+{
+	int len = 0, index;
+
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
+
+	_putchar('\n');
+}
