49d02b5be5c6eebe3ccf5ea4f9b78ac7328902b2
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-09-19 14:53:05 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,22 @@
-0-reset_to_98.c
+/*
+ * File: 2-strlen.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the length of.
+ *
+ * Return: The length of @str.
+ */
+size_t _strlen(const char *str)
+{
+	size_t length = 0;
+
+	while (*str++)
+		length++;
+
+	return (length);
+}
