82a6a967888f884c6dc18d54edbd466b3aaddb0b
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-09-19 14:54:26 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,18 @@
-0-reset_to_98.c
+/*
+ * File: 3-puts.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
