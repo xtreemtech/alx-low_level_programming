f0563d1967db01a74189ebda53f8ad17b59ebff9
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-09-27 00:50:49 +0100

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,1 +1,11 @@
-1-memcpy.c
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s: double pointer to be copied
+ * @to: where s gets set to
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
