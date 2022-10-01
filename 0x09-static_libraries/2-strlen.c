921ba8180ffd09bb5e4008231791d09c40bcd68c
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-10-01 08:50:49 +0100

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,23 +1,18 @@
-/*
- * File: 2-strlen.c
- * Auth: Brennan D Baraban
- */
-
 #include "main.h"
-#include <stddef.h>
-
 /**
- * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
- *
- * Return: The length of @str.
+ * _strlen - return the length of the string
+ *@s: char type pointer
+ *Return: length of string
  */
-size_t _strlen(const char *str)
+int _strlen(char *s)
 {
-	size_t length = 0;
+	int i, compt = 0;
 
-	while (*str++)
-		length++;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		compt++;
+	}
 
-	return (length);
+	return (compt);
+
 }
