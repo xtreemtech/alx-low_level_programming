745f8f744972422e3f116fb913b66b1464f91188
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-10-10 14:30:19 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,13 @@
-C - Preprocessor
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: Everything worked ok
+ */
+
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
