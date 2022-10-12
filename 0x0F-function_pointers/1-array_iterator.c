2439b03b1264527cf293784a9aa4715cf62ccb5c
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-10-12 14:39:59 +0100

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,1 +1,15 @@
-C - Function pointers
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_name - f points to either func in main
+ * @name: name
+ * @f: pointer to print upp or non upp func
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
