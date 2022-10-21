042fb510a9890babe179ee71ed0de07e00717b11
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-10-21 01:52:14 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,1 +1,12 @@
-C - Singly linked lists
+#include <stdio.h>
+
+/**
+ * first - function executed before main
+ * Return: no return.
+ */
+
+void __attribute__ ((constructor)) first()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
