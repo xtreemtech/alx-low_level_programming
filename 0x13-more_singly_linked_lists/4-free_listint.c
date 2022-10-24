50ca35127bc1110f8953c87660e4519aec146c1c
xtreemtech <108930783+xtreemtech@users.noreply.github.com>
2022-10-24 11:33:51 +0100

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,19 @@
-C - More singly linked lists
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: head of the list
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
